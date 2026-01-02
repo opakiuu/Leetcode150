7e1b6e28d6699a1ec41c40fe0b26d43fb6362c48
opakiuu <opakiuu@gmail.com>
2026-01-02 11:39:16 +0800

    88. Merge Sorted Array

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -8,8 +8,6 @@ using namespace std;
 
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
-
 // Example 1:
 
 // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -29,7 +27,6 @@ using namespace std;
 // Explanation: The arrays we are merging are [] and [1].
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
 
 // Constraints:
 
@@ -44,6 +41,9 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
+        for (int i = m + n; i > 0; i++)
+        {
+        }
         nums1 = {1, 2, 2, 3, 5, 6};
     }
 };
